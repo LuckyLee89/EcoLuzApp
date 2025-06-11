3f325bbaccf91505bbc71e2a17d7acfe997bb606
Lincon <linconp66@gmail.com>
2025-06-11 18:18:42 -0300

    Configuracao

---
+++ app.config.js
@@ -0,0 +1,39 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'EcoLuzApp',
+    slug: 'ecoluzapp',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+
+    // 👇 Aqui você expõe as variáveis do .env
+    extra: {
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    },
+  },
+};
+
--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+import Constants from 'expo-constants';
 
-const supabaseUrl = 'https://mboaulgjjxlmfvmoadde.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ib2F1bGdqanhsbWZ2bW9hZGRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1NzM0NTQsImV4cCI6MjA2NTE0OTQ1NH0.ytLN0wG0sD-o0KiC0JsrfEF1QZJCX0gV-r2RX8Kpwug';
+const supabaseUrl = Constants.expoConfig?.extra?.SUPABASE_URL!;
+const supabaseKey = Constants.expoConfig?.extra?.SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
