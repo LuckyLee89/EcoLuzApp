a3420897611b803311c213cfae7c026d8e664860
Lincon <linconp66@gmail.com>
2025-11-04 09:05:06 -0300

    remove

--- app.config.js
+++ app.config.js
@@ -52,11 +52,8 @@ export default {
       eas: {
         projectId: 'e44c6630-8ac6-4d60-b3c0-76b42a3463ac',
       },
-      //SUPABASE_URL: process.env.SUPABASE_URL,
-      //SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-      SUPABASE_URL: 'https://yjxijrddcydccheasvuh.supabase.co',
-      SUPABASE_ANON_KEY:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqeGlqcmRkY3lkY2NoZWFzdnVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5MTQ4NzEsImV4cCI6MjA3NzQ5MDg3MX0.KO2hzhqksWPbZzi5zRqvUcIsu58BnOGMwnGex5QG0PM',
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
     },
   },
 };
